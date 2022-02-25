bd6475b58ded4ececb42d5737f39530137eae334
unknown <hoanghuywww10@gmail.com>
2022-02-25 07:59:55 +0700

    Updates

---
+++ precache-manifest.50a9085859228e630fb0d93340f92acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcb0073f9b3a7dcabe47aa077e8024c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc5f3d445f36f26ae69b",
+    "url": "/static/css/main.fc5dfaa2.chunk.css"
+  },
+  {
+    "revision": "55907a7cb973491e0047",
+    "url": "/static/js/2.b7df0148.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.b7df0148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d58b8aa31344748448",
+    "url": "/static/js/3.fc39b07c.chunk.js"
+  },
+  {
+    "revision": "fc5f3d445f36f26ae69b",
+    "url": "/static/js/main.e0735e3e.chunk.js"
+  },
+  {
+    "revision": "8d0501c8667f84a30ed3",
+    "url": "/static/js/runtime-main.706f781d.js"
+  }
+]);
